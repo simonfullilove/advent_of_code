5c60c0ab42247036f31ed71ad6379431957b3c10
Simon Fullilove <simonx49@hotmail.com>
2021-12-29 16:03:22 +0000

    day1 upload and README update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # advent_of_code
-Contributions to Advent of Code 2021 - https://adventofcode.com/2021/about
+Contributions to Advent of Code 2021 - <a href="https://adventofcode.com/2021/about">Info</a>
--- day1.py
+++ day1.py
@@ -1,1 +1,18 @@
-print("hello world")
+# SONAR SWEEP
+
+# Part One
+
+input = 'data/day1_input'
+measurements = [int(l.strip()) for l in open(input).readlines()]
+increases = [measurements[i+1] > measurements[i] for i in range(len(measurements)-1)]
+res = sum(increases)
+print(res)
+
+
+# Part Two
+
+window_size = 3 # the size of the scanning window
+measurement_groups = [measurements[i:i+window_size] for i in range(len(measurements)-(window_size-1))]
+increases = [sum(measurement_groups[i+1]) > sum(measurement_groups[i]) for i in range(len(measurement_groups)-1)]
+res = sum(increases)
+print(res)
